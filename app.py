d4c6f3616dca5ff4f0715dc48113a11d92cf96d4
duj4 <boyu.du@yahoo.com>
2019-10-20 20:20:06 +0800

    re-construct app.py without default template of PyCharm

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+def hello():
+    return "Welcome to my watchlist!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0',port=5000)
